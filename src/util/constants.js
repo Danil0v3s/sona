aacc48faa0fd19f8f35f6ee6f2a101cc9413330c
Danilo Menezes <danilo.menezes@devoteam.com>
2019-06-17 09:57:01 +0100

    create constants and folders

---
+++ src/util/constants.js
@@ -0,0 +1,34 @@
+const RANKED = {
+    QUEUE: {
+        'SOLO_SR': 'RANKED_SOLO_5x5',
+        'FLEX_SR': 'RANKED_FLEX_SR',
+        'FLEX_TT': 'RANKED_FLEX_TT'
+    },
+    TIER: {
+        DIAMOND: 'DIAMOND', PLATINUM: 'PLATINUM', GOLD: 'GOLD', SILVER: 'SILVER', BRONZE: 'BRONZE', IRON: 'IRON'
+    },
+    DIVISION: {
+        I: 'I',
+        II: 'II',
+        III: 'III',
+        IV: 'IV'
+    }
+}
+
+const REGIONS = {
+    RU: 'RU',
+    KR: 'KR',
+    BR1: 'BR1',
+    OC1: 'OC1',
+    JP1: 'JP1',
+    NA1: 'NA1',
+    EUN1: 'EUN1',
+    EUW1: 'EUW1',
+    TR1: 'TR1',
+    LA1: 'LA1',
+    LA2: 'LA2'
+}
+
+module.exports = {
+    RANKED, REGIONS
+}
